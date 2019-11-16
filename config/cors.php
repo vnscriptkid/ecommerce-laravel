b8d542f9f6dec8fe0cea04578f5888d9d380baa6
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-16 20:45:49 +0700

    Add cors support for server

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+// php artisan vendor:publish --provider="Barryvdh\Cors\ServiceProvider"
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
