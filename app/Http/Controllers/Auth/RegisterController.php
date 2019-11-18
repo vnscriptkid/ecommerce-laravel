75be28729c3d628e560216dcaac037eb1073c190
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-19 02:50:28 +0700

    Add validation for user register api

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\PrivateUserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+
 class RegisterController extends Controller
 {
-    public function action(Request $request)
+    public function action(RegisterRequest $request)
     {
         $user = User::create($request->only(['name', 'email', 'password']));
         return new PrivateUserResource($user);
