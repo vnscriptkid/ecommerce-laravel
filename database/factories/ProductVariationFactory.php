6660f47d20dfd596c2f45b56c1910a00c552fb33
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-18 21:16:38 +0700

    Fix user factory and modify variation factory

--- database/factories/ProductVariationFactory.php
+++ database/factories/ProductVariationFactory.php
@@ -2,11 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use App\Models\ProductVariation;
 use Faker\Generator as Faker;
 
 $factory->define(ProductVariation::class, function (Faker $faker) {
     return [
-        'name' => $faker->unique()->name()
+        'name' => $faker->unique()->name(),
+        'product_id' => factory(Product::class)->create()->id
     ];
 });
