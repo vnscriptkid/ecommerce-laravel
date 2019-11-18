26f437d3269b84fcb842eb8a5953628ffddcb4e6
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-18 13:40:30 +0700

    Create `order` and `order_lines` tables

---
+++ database/migrations/2019_11_17_194024_create_order_lines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->unsignedInteger('product_variation_id')->index();
+            $table->unsignedInteger('order_id')->index();
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+
+            $table->foreign('product_variation_id')->references('id')->on('product_variations');
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+}
