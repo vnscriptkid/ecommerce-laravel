f150a630b475eb0996ee8fde4188cf9935f2bd99
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-18 21:14:22 +0700

    Fix creating view table in case left JOIN outputing null

--- database/migrations/2019_11_18_063641_create_product_variation_stock_view_table.php
+++ database/migrations/2019_11_18_063641_create_product_variation_stock_view_table.php
@@ -29,31 +29,33 @@ class CreateProductVariationStockViewTable extends Migration
     public function createView()
     {
         return "
-            CREATE VIEW product_variation_stock_view AS
+        CREATE VIEW product_variation_stock_view AS
+        SELECT
+            product_variations.product_id,
+            product_variations.id AS product_variation_id,
+            IFNULL(stocks.quantity, 0),
+            IFNULL(order_lines_grouped.ordered_quantity, 0),
+            COALESCE(IFNULL(stocks.quantity, 0) - IFNULL(order_lines_grouped.ordered_quantity, 0)) AS stock,
+            case when COALESCE(IFNULL(stocks.quantity, 0) - IFNULL(order_lines_grouped.ordered_quantity, 0)) > 0
+                THEN true
+                ELSE false
+            END in_stock
+        FROM product_variations
+        LEFT JOIN (
             SELECT
-                product_variations.product_id,
-                product_variations.id AS product_variation_id,
-                COALESCE(stocks.quantity - order_lines_grouped.order_quantity, 0) AS stock,
-                case when COALESCE(stocks.quantity - order_lines_grouped.order_quantity, 0) > 0
-                    THEN true
-                    ELSE false
-                END in_stock
-            FROM product_variations
-            LEFT JOIN (
-                SELECT
-                    SUM(stocks.quantity) AS quantity,
-                    stocks.product_variation_id AS product_variation_id
-                FROM stocks
-                GROUP BY stocks.product_variation_id
-            ) AS stocks
-            ON stocks.product_variation_id = product_variations.id
-            LEFT JOIN (
-                SELECT
-                    SUM(order_lines.quantity) AS order_quantity,
-                    order_lines.product_variation_id AS id
-                FROM order_lines
-                GROUP BY order_lines.product_variation_id
-            ) AS order_lines_grouped USING (id);
+                IFNULL(SUM(stocks.quantity), 0) AS quantity,
+                stocks.product_variation_id AS product_variation_id
+            FROM stocks
+            GROUP BY stocks.product_variation_id
+        ) AS stocks
+        ON stocks.product_variation_id = product_variations.id
+        LEFT JOIN (
+            SELECT
+                IFNULL(SUM(order_lines.quantity), 0) AS ordered_quantity,
+                order_lines.product_variation_id AS id
+            FROM order_lines
+            GROUP BY order_lines.product_variation_id
+        ) AS order_lines_grouped USING (id);
         ";
     }
 
